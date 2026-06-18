c8660e50d44b9f85e53bfc129909495862dc1496
Ahmad Fawad Amiri <ahmadfawad.amiri1@gmail.com>
2026-06-18 08:36:05 +0200

    create sawgger

--- src/main/java/com/afa/demo0001/security/SecurityConfig.java
+++ src/main/java/com/afa/demo0001/security/SecurityConfig.java
@@ -34,14 +34,13 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.DELETE, "/api/user/**").hasAuthority("ROLE_CHIEF")
-                        .requestMatchers(HttpMethod.POST, "/api/products/**").hasAnyAuthority("ROLE_CHIEF", "ROLE_ADMIN")
-                        .requestMatchers(HttpMethod.POST, "/api/categories/**").hasAnyAuthority("ROLE_CHIEF", "ROLE_ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/api/products/**").hasAnyAuthority("ROLE_CHIEF", "ROLE_ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/api/categories/**").hasAnyAuthority("ROLE_CHIEF", "ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/products/**", "/api/categories/**").hasAnyAuthority("ROLE_CHIEF", "ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/products/**", "/api/categories/**").hasAnyAuthority("ROLE_CHIEF", "ROLE_ADMIN")
 
-                        .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/categories/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/products/**", "/api/categories/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
+
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
 
                         .anyRequest().authenticated());
 
